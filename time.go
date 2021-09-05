db42505e164d9c17611842d4819b00cb451f677b
Fufu <fufuok@qq.com>
2021-09-05 13:51:59 +0800

    Update: clean up code of time

--- time.go
+++ time.go
@@ -12,8 +12,7 @@ func WaitNextMinute() {
 
 // Get0Hour 当天 0 点
 func Get0Hour(t time.Time) time.Time {
-	y, m, d := t.Date()
-	return time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
 // Get0Yesterday 昨天 0 点
@@ -21,22 +20,19 @@ func Get0Yesterday(t time.Time) time.Time {
 	return Get0Hour(t.AddDate(0, 0, -1))
 }
 
-// Get0Tomorrow 昨天 0 点
+// Get0Tomorrow 明天 0 点
 func Get0Tomorrow(t time.Time) time.Time {
 	return Get0Hour(t.AddDate(0, 0, 1))
 }
 
 // Get0Minute 0 分
 func Get0Minute(t time.Time) time.Time {
-	y, m, d := t.Date()
-	return time.Date(y, m, d, t.Hour(), 0, 0, 0, t.Location())
+	return t.Truncate(time.Hour)
 }
 
 // Get0Second 0 秒
 func Get0Second(t time.Time) time.Time {
-	t.Truncate(time.Minute)
-	y, m, d := t.Date()
-	return time.Date(y, m, d, t.Hour(), t.Minute(), 0, 0, t.Location())
+	return t.Truncate(time.Minute)
 }
 
 // Get0Week 本周一 0 点
@@ -61,8 +57,7 @@ func Get0NextWeek(t time.Time) time.Time {
 
 // Get0Month 当月第一天 0 点
 func Get0Month(t time.Time) time.Time {
-	y, m, _ := t.Date()
-	return time.Date(y, m, 1, 0, 0, 0, 0, t.Location())
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
 }
 
 // Get0LastMonth 上月第一天 0 点
