4f027b3e0b42f0fe1af43e57fdb5d8b8e319c033
Fufu <fufuok@qq.com>
2022-05-14 15:18:14 +0800

    feat: add no copy and uncomparable struct

---
+++ nocmp_test.go
@@ -0,0 +1,149 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package utils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestNocmpComparability(t *testing.T) {
+	tests := []struct {
+		desc       string
+		give       interface{}
+		comparable bool
+	}{
+		{
+			desc: "NoCmp struct",
+			give: NoCmp{},
+		},
+		{
+			desc: "struct with NoCmp embedded",
+			give: struct{ NoCmp }{},
+		},
+		{
+			desc:       "pointer to struct with NoCmp embedded",
+			give:       &struct{ NoCmp }{},
+			comparable: true,
+		},
+
+		// All exported types must be uncomparable.
+		{desc: "Bool", give: Bool{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			typ := reflect.TypeOf(tt.give)
+			AssertEqualf(t, tt.comparable, typ.Comparable(),
+				"type %v comparablity mismatch", typ)
+		})
+	}
+}
+
+// NoCmp must not add to the size of a struct in-memory.
+func TestNocmpSize(t *testing.T) {
+	type x struct{ _ int }
+
+	before := reflect.TypeOf(x{}).Size()
+
+	type y struct {
+		_ NoCmp
+		_ x
+	}
+
+	after := reflect.TypeOf(y{}).Size()
+
+	AssertEqual(t, before, after,
+		"expected NoCmp to have no effect on struct size")
+}
+
+// This test will fail to compile if we disallow copying of NoCmp.
+//
+// We need to allow this so that users can do,
+//
+//   var x atomic.Int32
+//   x = atomic.NewInt32(1)
+func TestNocmpCopy(t *testing.T) {
+	type foo struct{ _ NoCmp }
+
+	t.Run("struct copy", func(t *testing.T) {
+		a := foo{}
+		b := a
+		_ = b // unused
+	})
+
+	t.Run("pointer copy", func(t *testing.T) {
+		a := &foo{}
+		b := *a
+		_ = b // unused
+	})
+}
+
+// Fake go.mod with no dependencies.
+const _exampleGoMod = `module example.com/nocmp`
+
+const _badFile = `package utils 
+import "fmt"
+type Int64 struct {
+	_ NoCmp
+	v int64
+}
+func shouldNotCompile() {
+	var x, y Int64
+	fmt.Println(x == y)
+}
+`
+
+func TestNocmpIntegration(t *testing.T) {
+	tempdir, err := ioutil.TempDir("", "nocmp")
+	AssertEqual(t, true, err == nil, "unable to set up temporary directory")
+	defer os.RemoveAll(tempdir)
+
+	Nocmp, err := ioutil.ReadFile("nocmp.go")
+	AssertEqual(t, true, err == nil, "unable to read nocmp.go")
+
+	err = ioutil.WriteFile(filepath.Join(tempdir, "go.mod"), []byte(_exampleGoMod), 0644)
+	AssertEqual(t, true, err == nil, "unable to write go.mod")
+
+	err = ioutil.WriteFile(filepath.Join(tempdir, "nocmp.go"), Nocmp, 0644)
+	AssertEqual(t, true, err == nil, "unable to write NoCmp.go")
+
+	err = ioutil.WriteFile(filepath.Join(tempdir, "bad.go"), []byte(_badFile), 0644)
+	AssertEqual(t, true, err == nil, "unable to write bad.go")
+
+	var stderr bytes.Buffer
+	cmd := exec.Command("go", "build")
+	cmd.Dir = tempdir
+	// Create a minimal build enviroment with only HOME set so that "go
+	// build" has somewhere to put the cache and other Go files in.
+	cmd.Env = []string{"HOME=" + filepath.Join(tempdir, "home")}
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	AssertEqual(t, true, err != nil, "bad.go must not compile")
+	ok := strings.Contains(stderr.String(), "struct containing NoCmp cannot be compared")
+	AssertEqual(t, true, ok, stderr.String())
+}
