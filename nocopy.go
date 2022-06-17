0d2aa56b1db25ec1ffb541f567df5ede62d12866
Fufu <fufuok@qq.com>
2022-06-17 08:14:02 +0800

    chore: nolint:unused

--- nocopy.go
+++ nocopy.go
@@ -3,12 +3,12 @@ package utils
 // NoCopy may be added to structs which must not be copied
 // after the first use.
 //
-// See https://golang.org/issues/8005#issuecomment-190753527
-// for details.
+// See https://github.com/golang/go/issues/8005#issuecomment-190753527 for details.
+// and also: https://stackoverflow.com/questions/52494458/nocopy-minimal-example
 //
 // Note that it must not be embedded, due to the Lock and Unlock methods.
-type NoCopy struct{}
+type NoCopy struct{} //nolint:unused
 
 // Lock is a no-op used by -copylocks checker from `go vet`.
-func (*NoCopy) Lock()   {}
-func (*NoCopy) Unlock() {}
+func (*NoCopy) Lock()   {} //nolint:unused
+func (*NoCopy) Unlock() {} //nolint:unused
