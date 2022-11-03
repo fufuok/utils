055e6f4a1b5573b22470fa3c716b18bc9d99a9d9
Fufu <fufuok@qq.com>
2022-11-03 08:29:11 +0800

    feat: add binary helper

---
+++ xbinary/endian.go
@@ -0,0 +1,16 @@
+package xbinary
+
+import (
+	"unsafe"
+)
+
+// IsLittleEndian 判断系统是否为小端序
+func IsLittleEndian() bool {
+	var i int32 = 1
+	return *(*byte)(unsafe.Pointer(&i)) == 1
+}
+
+// SwapEndianUint32 大小端交换
+func SwapEndianUint32(val uint32) uint32 {
+	return (val&0xff000000)>>24 | (val&0x00ff0000)>>8 | (val&0x0000ff00)<<8 | (val&0x000000ff)<<24
+}
