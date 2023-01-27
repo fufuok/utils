88a61fa57a86aa314a5492930bba043d112b3070
Fufu <fufuok@qq.com>
2023-01-27 16:36:00 +0800

    sync: fix unicode handling in trie by @zyedidia

--- generic/trie/trie.go
+++ generic/trie/trie.go
@@ -21,6 +21,52 @@ type node[V any] struct {
 	valid            bool
 }
 
+func (n *node[V]) isUnused() bool {
+	return !n.valid && n.mid == nil
+}
+
+func (n *node[V]) delete() *node[V] {
+	if n == nil {
+		return nil
+	}
+	if n.right == nil {
+		return n.left
+	}
+	if n.left == nil {
+		return n.right
+	}
+
+	deleted := n
+	n = deleted.right.minChild()
+	n.right = deleted.right.deleteMinChild()
+	n.left = deleted.left
+	return n
+}
+
+func (n *node[V]) minChild() *node[V] {
+	if n == nil {
+		return nil
+	}
+
+	for n.left != nil {
+		n = n.left
+	}
+	return n
+}
+
+func (n *node[V]) deleteMinChild() *node[V] {
+	if n == nil {
+		return nil
+	}
+
+	if n.left == nil { // n is the min node
+		return n.right
+	}
+
+	n.left = n.left.deleteMinChild()
+	return n
+}
+
 // New returns an empty trie.
 func New[V any]() *Trie[V] {
 	return &Trie[V]{}
@@ -76,21 +122,10 @@ func (t *Trie[V]) Put(key string, val V) {
 	if !t.Contains(key) {
 		t.n++
 	}
-	t.root = t.put(t.root, key, val, 0, true)
+	t.root = t.put(t.root, key, val, 0)
 }
 
-// Remove removes the value associated with 'key'.
-func (t *Trie[V]) Remove(key string) {
-	if len(key) == 0 || !t.Contains(key) {
-		return
-	}
-	var v V
-	t.n--
-	// put a tombstone into the deleted value's node
-	t.root = t.put(t.root, key, v, 0, false)
-}
-
-func (t *Trie[V]) put(x *node[V], key string, val V, d int, valid bool) *node[V] {
+func (t *Trie[V]) put(x *node[V], key string, val V, d int) *node[V] {
 	c := key[d]
 	if x == nil {
 		x = &node[V]{
@@ -98,15 +133,51 @@ func (t *Trie[V]) put(x *node[V], key string, val V, d int, valid bool) *node[V]
 		}
 	}
 	if c < x.c {
-		x.left = t.put(x.left, key, val, d, valid)
+		x.left = t.put(x.left, key, val, d)
 	} else if c > x.c {
-		x.right = t.put(x.right, key, val, d, valid)
+		x.right = t.put(x.right, key, val, d)
 	} else if d < len(key)-1 {
-		x.mid = t.put(x.mid, key, val, d+1, valid)
+		x.mid = t.put(x.mid, key, val, d+1)
 	} else {
 		x.val = val
-		x.valid = valid
+		x.valid = true
 	}
+	return x
+}
+
+// Remove removes the value associated with 'key', along with any nodes of the key that are no
+// longer used.
+func (t *Trie[V]) Remove(key string) {
+	if len(key) == 0 {
+		return
+	}
+
+	t.root = t.remove(t.root, key, 0)
+	t.n--
+}
+
+func (t *Trie[V]) remove(x *node[V], key string, d int) *node[V] {
+	if x == nil {
+		return nil
+	}
+
+	c := key[d]
+	if c < x.c {
+		x.left = t.remove(x.left, key, d)
+	} else if c > x.c {
+		x.right = t.remove(x.right, key, d)
+	} else if d < len(key)-1 {
+		x.mid = t.remove(x.mid, key, d+1)
+	} else {
+		var v V
+		x.val = v
+		x.valid = false
+	}
+
+	if x.isUnused() {
+		return x.delete()
+	}
+
 	return x
 }
 
@@ -137,7 +208,7 @@ func (t *Trie[V]) LongestPrefix(query string) string {
 
 // Keys returns all keys in the trie.
 func (t *Trie[V]) Keys() (queue []string) {
-	return t.collect(t.root, "", queue)
+	return t.collect(t.root, nil, queue)
 }
 
 // KeysWithPrefix returns all keys with prefix 'prefix'.
@@ -152,17 +223,17 @@ func (t *Trie[V]) KeysWithPrefix(prefix string) (queue []string) {
 	if x.valid {
 		queue = []string{prefix}
 	}
-	return t.collect(x.mid, prefix, queue)
+	return t.collect(x.mid, []byte(prefix), queue)
 }
 
-func (t *Trie[V]) collect(x *node[V], prefix string, queue []string) []string {
+func (t *Trie[V]) collect(x *node[V], prefix []byte, queue []string) []string {
 	if x == nil {
 		return queue
 	}
 	queue = t.collect(x.left, prefix, queue)
 	if x.valid {
-		queue = append(queue, prefix+string(x.c))
+		queue = append(queue, string(append(prefix, x.c)))
 	}
-	queue = t.collect(x.mid, prefix+string(x.c), queue)
+	queue = t.collect(x.mid, append(prefix, x.c), queue)
 	return t.collect(x.right, prefix, queue)
 }
