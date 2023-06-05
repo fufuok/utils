03df9312f8a11ad6a4086ad5ca12f1c6764edb53
Fufu <fufuok@qq.com>
2023-06-05 09:21:27 +0800

    sync: xsync v2.4.1

---
+++ xsync/util_test.go
@@ -0,0 +1,53 @@
+package xsync_test
+
+import (
+	"math/rand"
+	"testing"
+
+	. "github.com/fufuok/utils/xsync"
+)
+
+func TestNextPowOf2(t *testing.T) {
+	if NextPowOf2(0) != 1 {
+		t.Error("nextPowOf2 failed")
+	}
+	if NextPowOf2(1) != 1 {
+		t.Error("nextPowOf2 failed")
+	}
+	if NextPowOf2(2) != 2 {
+		t.Error("nextPowOf2 failed")
+	}
+	if NextPowOf2(3) != 4 {
+		t.Error("nextPowOf2 failed")
+	}
+}
+
+// This test is here to catch potential problems
+// with fastrand-related changes.
+func TestFastrand(t *testing.T) {
+	count := 10000
+	set := make(map[uint32]struct{}, count)
+
+	for i := 0; i < count; i++ {
+		num := Fastrand()
+		set[num] = struct{}{}
+	}
+
+	if len(set) != count {
+		t.Error("duplicated rand num")
+	}
+}
+
+func BenchmarkFastrand(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Fastrand()
+	}
+	// about 1.4 ns/op on x86-64
+}
+
+func BenchmarkRand(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = rand.Uint32()
+	}
+	// about 12 ns/op on x86-64
+}
