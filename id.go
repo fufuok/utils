409e33252d7a5bc810cadff136189751b255b1ea
Fufu <fufuok@qq.com>
2023-10-09 13:55:27 +0800

    feat: add Goroutine ID()

--- id.go
+++ id.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"bytes"
+	"runtime"
+	"strconv"
 	"sync/atomic"
 )
 
@@ -9,4 +12,14 @@ var runtimeID uint64
 // ID 运行时自增 ID (每次程序启动从 1 开始)
 func ID() uint64 {
 	return atomic.AddUint64(&runtimeID, 1)
+}
+
+// GoroutineID 获取 Goroutine ID
+func GoroutineID() (uint64, error) {
+	b := make([]byte, 64)
+	n := runtime.Stack(b, false)
+	b = b[:n]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	return strconv.ParseUint(string(b), 10, 64)
 }
--- id_test.go
+++ id_test.go
@@ -12,3 +12,18 @@ func TestID(t *testing.T) {
 	b := utils.ID()
 	assert.Equal(t, b, a+1)
 }
+
+func TestGoroutineID(t *testing.T) {
+	gid, err := utils.GoroutineID()
+	assert.Equal(t, nil, err)
+	t.Log(gid)
+}
+
+func BenchmarkGoroutineID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := utils.GoroutineID()
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
