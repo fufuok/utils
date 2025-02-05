485e7f7b9c3e24c60d5b313beb4a97b672ba2422
Fufu <fufuok@qq.com>
2025-02-05 17:19:22 +0800

    chore: fix typos

--- trylock_test.go
+++ trylock_test.go
@@ -33,7 +33,7 @@ func TestTrylock(t *testing.T) {
 	}()
 
 	ts := time.Now()
-	ok = m.TryLock(100 * time.Millisecond)
+	ok = m.TryLock(120 * time.Millisecond)
 	took := time.Since(ts)
 	if !ok {
 		t.Fatal("it should be the lock succeeded but failed")
